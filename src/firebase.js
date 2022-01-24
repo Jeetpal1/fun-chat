bdee896b92bebf46795b61a5c9ab0f0bf5c15709
Jeetpal <singhjeetpal99@gmail.com>
2022-01-24 01:48:48 -0600

    react init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAISIoGw4Ia6AukSV7KcAEfnFQ3VLYhTFI",
+    authDomain: "fun-chat-acc4a.firebaseapp.com",
+    projectId: "fun-chat-acc4a",
+    storageBucket: "fun-chat-acc4a.appspot.com",
+    messagingSenderId: "657963663171",
+    appId: "1:657963663171:web:9d3581d29534bb52a039f6",
+    measurementId: "G-RMHVJYX5N5"
+  };
